f6d59fa0c175b45bb0014405b873b65e072e9481
bisquickman231 <whuskey6@gmail.ocm>
2019-03-15 01:35:43 -0600

    Documented parse_output.

--- tunnel_rpc/methods.py
+++ tunnel_rpc/methods.py
@@ -38,6 +38,13 @@ def eval_commands(client, cont, commands):
 
 
 def parse_output(output):
+    """Store all terminal lines associated with the program run commands.
+
+    Args:
+        output(str): Container with pre-run commands in terminal
+    Returns:
+        (list) The contents of the terminal
+    """
     preamble = True
     commands = []
     buffer = []
